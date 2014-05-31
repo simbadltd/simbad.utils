ed3878492dc47442fe706b2c41ea0f59bc8963a6
Konstantin Kryzhanovskii <kostyna@bk.ru>
2014-05-31 15:23:34 +0700

    Added useful methods

---
+++ Simbad.Utils.Silverlight/Converters/NullableBooleanToVisibilityConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simbad.Utils.Silverlight.Converters
+{
+    class NullableBooleanToVisibilityConverter
+    {
+    }
+}
