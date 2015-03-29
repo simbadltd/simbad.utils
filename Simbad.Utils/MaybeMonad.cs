71cbc701690e612ae4e735a26cb25f5e2307d203
Konstantin Kryzhanovskii <kostyna@bk.ru>
2015-03-29 17:52:36 +0600

    new functionality

---
+++ Simbad.Utils/MaybeMonad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Simbad.Utils
+{
+    public static class MaybeMonad
+    {
+        public static TOut IfNotNull<TIn, TOut>(this TIn obj, Func<TIn, TOut> selector)
+            where TIn : class
+            where TOut : class
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            return selector(obj);
+        }
+
+        public static TOut IfNotNull<TIn, TOut>(this TIn obj, Func<TIn, TOut> selector, TOut? defaultValue)
+            where TIn : class
+            where TOut : struct
+        {
+            if (obj == null)
+            {
+                return defaultValue.HasValue ? defaultValue.Value : default(TOut);
+            }
+
+            return selector(obj);
+        }
+    }
+}
